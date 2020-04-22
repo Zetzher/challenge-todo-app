153c453c9bcda4594a00a11b2ed7f0b9bd91835d
Zetzher <fyeah16@gmail.com>
2020-04-22 13:21:24 +0200

    getarea fixed

--- src/pages/Getarea.js
+++ src/pages/Getarea.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
-class Getarea extends Component () {
-    constructor(props){
-    super(props)
-        this.state = {
-            idtarea : this.props.match.params.id
-        }
-    }
+class Getarea extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        id: this.props.match.params.id,
+        title: "",
+        body: ""
+      };
+}
 
     render(){
         return (
             <>
                 <div className="display-tareas">
                     <h1>Tareas traidas de la API:</h1>
-                    <h4>{}</h4>
-                    <h4>{}</h4> 
+                    <h4></h4>
+                    <h4></h4> 
                 </div>
             </>
     )}
